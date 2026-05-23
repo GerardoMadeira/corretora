5c209b5e71469a5e3e2e936bab0a1d870b5eda1e
Gerardo Madeira <gerardomadeira@outlook.com>
2026-05-23 00:48:16 -0300

    estrutura inicial do projeto

---
+++ apps/apolices/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApolicesConfig(AppConfig):
+    name = "apps.apolices"
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from corretora!")
+
+
+if __name__ == "__main__":
+    main()
