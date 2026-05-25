0b99573aed1f8dc8d15b0c278ef9efe41e68ca46
Gerardo Madeira <gerardomadeira@outlook.com>
2026-05-25 20:00:27 -0300

    adicionado comando de inicializacao startup

--- config/settings.py
+++ config/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = []
+if (BASE_DIR / "static").exists():
+    STATICFILES_DIRS.append(BASE_DIR / "static")
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
