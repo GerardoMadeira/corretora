07f40c3a26b5d8b9c0486d54eafc312aa0dcd3a8
Gerardo Madeira <gerardomadeira@outlook.com>
2026-05-25 21:30:33 -0300

    Configuração definitiva do WhiteNoise para produção2

--- config/settings.py
+++ config/settings.py
@@ -156,7 +156,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": ("whitenoise.storage.CompressedManifestStaticFilesStorage"),
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
