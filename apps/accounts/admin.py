af3f02421d326999e4d4ec18dc3d51c5c98a0fb9
Gerardo Madeira <gerardomadeira@outlook.com>
2026-05-28 21:59:41 -0300

    mudanca home

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,82 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    ordering = ("email",)
+
+    list_display = (
+        "email",
+        "nome",
+        "nome_corretora",
+        "is_staff",
+        "is_active",
+    )
+
+    search_fields = (
+        "email",
+        "nome",
+        "nome_corretora",
+    )
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "password",
+                )
+            },
+        ),
+        (
+            "Informações",
+            {
+                "fields": (
+                    "nome",
+                    "nome_corretora",
+                    "telefone",
+                )
+            },
+        ),
+        (
+            "Permissões",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Datas importantes",
+            {"fields": ("last_login",)},
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "nome",
+                    "nome_corretora",
+                    "telefone",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
